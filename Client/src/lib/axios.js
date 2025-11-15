d49c1ddfe306c68503a8e993aaefe52ecb2c5ba9
Shweta <vermashweta9138@gmail.com>
2025-11-15 17:59:13 +0530

    Update axios.js

--- Client/src/lib/axios.js
+++ Client/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://35.175.186.44:5001/api" : "/api";
+const BASE_URL = "http://35.175.186.44:5001";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
